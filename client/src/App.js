8e9132b97cc3471bf97ace5ec6670f7f0466e946
Dexter <dexterdevmode@gmail.com>
2022-10-23 09:40:34 +0530

    Initial setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div className="h-screen w-full p-3 bg-gradient-to-br from-indigo-500 via-purple-500 to-cyan-300">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between width-full bg-[#72F3DC]/[0.4] p-3">
+      <div className="title">CRYPO FUNDER</div>
+      <div className="flex items-center">
+        <span>0xa73b739fad739da812fb54cd</span>
+        <span className="p-4 bg-slate-300 rounded-full"></span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
