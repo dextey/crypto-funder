a41b176b7eb3c64dc6e9ef2b18b7680efcf8dfed
Dexter <dexterdevmode@gmail.com>
2022-10-25 23:17:53 +0530

    Added list of funders to UI

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Main from "./Components/Main";
 import Navbar from "./Components/Navbar";
 
 import { ethers } from "ethers";
-import { ABI, contractAddress } from "./Constants";
+import { CONTRACT } from "./contracts/contract";
 
 function App() {
   const [wallet, setWallet] = useState(null);
@@ -18,18 +18,22 @@ function App() {
       if (provider) {
         const signer = provider.getSigner();
 
-        const contract = new ethers.Contract(contractAddress, ABI, signer);
+        const fundMe = new ethers.Contract(
+          CONTRACT.address,
+          CONTRACT.abi,
+          signer
+        );
 
-        setContract(contract);
+        setContract(fundMe);
       }
     }
   }, []);
 
   return (
-    <div className="h-screen w-full p-3 bg-gradient-to-br from-[#131130] via-[#171368dd] to-[#00ffff] flex flex-col">
+    <div className="h-screen w-full p-3 bg-gradient-to-br from-[#131130] via-[#171368dd] to-[#00ffff] flex flex-col overflow-hidden">
       <Navbar wallet={wallet} setWallet={setWallet} />
       <div className="flex h-full py-4 justify-between">
-        <Funders />
+        <Funders contract={contract} />
         <Main contract={contract} wallet={wallet} />
       </div>
     </div>
--- client/src/Components/Funders.jsx
+++ client/src/Components/Funders.jsx
@@ -1,7 +1,82 @@
-const Funders = () => {
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+
+const Funders = ({ contract }) => {
+  const [funders, setFunders] = useState([
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
+  ]);
+
+  const getFunders = async () => {
+    const data = await contract.getFunders();
+    return data;
+  };
+
+  useEffect(() => {
+    contract &&
+      getFunders().then((_funders) => {
+        _funders.map(async (_funder) => {
+          const amount = await contract.fundedByAddress(_funder);
+          setFunders((prev) => [
+            ...prev,
+            { address: _funder, amount: ethers.utils.formatEther(amount) },
+          ]);
+        });
+      });
+  }, [contract]);
+
+  funders.length && console.log(funders);
+
   return (
-    <div className="flex flex-col w-[25%]  text-[#0ff] shadow-xl rounded-sm shadow-[#1e2546]  ">
-      <div className=" text-[2.3rem] font-extrabold">Top_Funders</div>
+    <div className="flex flex-col w-[25%] text-[#0ff] shadow-xl rounded-sm shadow-[#1e2546]">
+      <div className=" text-[2.3rem] p-3 font-extrabold   animate-bounce ">
+        <span>Top_Funders</span>
+      </div>
+
+      <div className="flex  flex-col overflow-y-scroll overflow-x-hidden h-full  ">
+        {funders.map((funder, index) => {
+          return (
+            <div
+              key={index}
+              className="flex text-[1.3rem] my-1 bg-[#00ffff28] items-center"
+            >
+              <span>
+                {funder.address.slice(0, 8) +
+                  "...." +
+                  funder.address.slice(30, 42)}
+              </span>
+              <span className="mx-2 font-extrabold bg-white rounded-full p-1 px-3">
+                {funder.amount}ETH
+              </span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
