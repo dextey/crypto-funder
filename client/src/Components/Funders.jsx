6d38fa152fded21dc5d81399fd2d14c93750c79b
Dexter <dexterdevmode@gmail.com>
2022-10-31 16:45:07 +0530

    List of funderes fetched from blockchain

--- client/src/Components/Funders.jsx
+++ client/src/Components/Funders.jsx
@@ -50,8 +50,6 @@ const Funders = ({ contract }) => {
       });
   }, [contract]);
 
-  funders.length && console.log(funders);
-
   return (
     <div className="flex flex-col w-[25%] text-[#0ff] shadow-xl rounded-sm border-2 border-double ">
       <div className=" text-[2.3rem] p-3 font-extrabold  bg-yellow-100 border-2 ">
@@ -63,17 +61,19 @@ const Funders = ({ contract }) => {
           return (
             <div
               key={index}
-              className="flex text-[1.4rem] font-extrabold my-2  w-fit p-2
-               items-center"
+              className="flex  text-[1.2rem]   mb-1 w-full items-center"
             >
-              <span>
-                {funder.address.slice(0, 8) +
-                  "...." +
-                  funder.address.slice(30, 42)}
-              </span>
-              <span className="mx-2  bg-yellow-200 text-black rounded-full p-1 px-3">
-                {funder.amount}ETH
-              </span>
+              <div className="flex  w-9/12 font-extrabold my-2  p-2 items-center">
+                <span>
+                  {funder.address.slice(0, 10) +
+                    "...." +
+                    funder.address.slice(30, 42)}
+                </span>
+              </div>
+
+              <div className="flex my-2 w-3/12 items-center mx-2 justify-center bg-yellow-200 text-black rounded-full  p-2">
+                {funder.amount} ETH
+              </div>
             </div>
           );
         })}
