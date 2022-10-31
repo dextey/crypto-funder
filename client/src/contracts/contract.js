e2894c2781772d8c71e034c376d6dbef4bbfb3c5
Dexter <dexterdevmode@gmail.com>
2022-10-31 16:48:06 +0530

    Hardhat contract - ABI and Address

---
+++ client/src/contracts/contract.js
@@ -0,0 +1,107 @@
+export const CONTRACT = {
+  address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+  abi: [
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "priceFeedAddress",
+          type: "address",
+        },
+      ],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      inputs: [],
+      name: "FundMe_error",
+      type: "error",
+    },
+    {
+      stateMutability: "payable",
+      type: "fallback",
+    },
+    {
+      inputs: [],
+      name: "fund",
+      outputs: [],
+      stateMutability: "payable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      name: "fundedByAddress",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "funders",
+      outputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "getFunders",
+      outputs: [
+        {
+          internalType: "address[]",
+          name: "",
+          type: "address[]",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "priceFeed",
+      outputs: [
+        {
+          internalType: "contract AggregatorV3Interface",
+          name: "",
+          type: "address",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "withDraw",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      stateMutability: "payable",
+      type: "receive",
+    },
+  ],
+};
