5cf0e03cb8489dd59ae5744bc60ba07de72bffd7
Dexter <dexterdevmode@gmail.com>
2022-11-01 23:31:04 +0530

    Added funder sorting and view balance

--- client/src/Components/Funders.jsx
+++ client/src/Components/Funders.jsx
@@ -1,34 +1,8 @@
 import { ethers } from "ethers";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
-const Funders = ({ contract }) => {
+const Funders = ({ contract, transactionReceipt }) => {
   const [funders, setFunders] = useState([
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
-    { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
     { address: "0xf39Fd6kljsdlkansfdgsdfgwsrg79cffFb92266", amount: "10" },
   ]);
 
@@ -48,14 +22,24 @@ const Funders = ({ contract }) => {
           ]);
         });
       });
-  }, [contract]);
+  }, [contract, transactionReceipt]);
 
-  return (
-    <div className="flex flex-col w-[25%] text-[#0ff] shadow-xl rounded-sm border-2 border-double ">
-      <div className=" text-[2.3rem] p-3 font-extrabold  bg-yellow-100 border-2 ">
-        <span className="animate-bounce text-black">Top_Funders</span>
-      </div>
+  const sort = () => {
+    function compare(a, b) {
+      if (parseFloat(a.amount) < parseFloat(b.amount)) {
+        return 1;
+      }
+      if (parseFloat(a.amount) > parseFloat(b.amount)) {
+        return -1;
+      }
+      return 0;
+    }
 
+    setFunders(() => [...funders.sort(compare)]);
+  };
+
+  const list = useMemo(() => {
+    return (
       <div className="flex  flex-col overflow-y-scroll overflow-x-hidden h-full  ">
         {funders.map((funder, index) => {
           return (
@@ -63,21 +47,33 @@ const Funders = ({ contract }) => {
               key={index}
               className="flex  text-[1.2rem]   mb-1 w-full items-center"
             >
-              <div className="flex  w-9/12 font-extrabold my-2  p-2 items-center">
+              <div className="flex  w-8/12 font-extrabold my-2  p-2 items-center">
                 <span>
-                  {funder.address.slice(0, 10) +
+                  {funder.address.slice(0, 9) +
                     "...." +
-                    funder.address.slice(30, 42)}
+                    funder.address.slice(33, 42)}
                 </span>
               </div>
 
-              <div className="flex my-2 w-3/12 items-center mx-2 justify-center bg-yellow-200 text-black rounded-full  p-2">
+              <div className="flex my-2 text-1xl w-4/12 items-center mx-3 justify-center bg-yellow-200 text-black rounded-full  py-2">
                 {funder.amount} ETH
               </div>
             </div>
           );
         })}
       </div>
+    );
+  }, [funders]);
+
+  return (
+    <div className="flex flex-col w-[25%] text-[#0ff] shadow-xl rounded-sm border-2 border-double ">
+      <div className=" text-[2.3rem] p-3 font-extrabold  bg-yellow-100 border-2 ">
+        <span className="animate-bounce text-black">Top_Funders</span>
+        <button className="p-4 " onClick={sort}>
+          Sort
+        </button>
+      </div>
+      {list}
     </div>
   );
 };
--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -1,12 +1,15 @@
 import { ethers } from "ethers";
 import { useEffect, useRef, useState } from "react";
 
-const Main = ({ contract, wallet }) => {
+const Main = ({
+  contract,
+  wallet,
+  transactionReceipt,
+  setTransactionReceipt,
+}) => {
   const value = useRef();
 
   const [contractBalance, setContractBalance] = useState(0);
-
-  const [transactionReceipt, setTransactionReceipt] = useState("");
 
   const fund = async () => {
     const ethAmount = value.current.value;
@@ -29,25 +32,27 @@ const Main = ({ contract, wallet }) => {
     // const res = await contract.s_addressFunded(wallet);
     const response = await provider.getBalance(contract.address);
 
-    return ethers.utils.formatEther(response);
+    setContractBalance(ethers.utils.formatEther(response));
     //   "ete " + ethers.utils.formatEther(res)
   };
 
   useEffect(() => {
-    getBalance().then((balance) => setContractBalance(balance));
-  }, [transactionReceipt]);
+    getBalance();
+  }, [transactionReceipt, contract]);
 
   return (
-    <div className="flex w-[73%] bg-white bg-opacity-20  text-white backdrop-blur-sm">
+    <div className="flex w-[73%] bg-white bg-opacity-20  text-white backdrop-blur-sm overflow-y-scroll">
       <div className="flex flex-col font-bold px-6 pt-5">
         <div className="flex w-full justify-between items-center mb-2">
           <div className="text-[3rem]">Welcome cypto_funders</div>
-          <div className="text-[2rem] mx-2 ">Balance : {contractBalance}</div>
+          <div className="text-[2rem] mx-2 ">
+            Balance : {contractBalance} ETH
+          </div>
         </div>
-        <div className="flex overflow-y-scroll">
+        <div className="flex ">
           <div className="flex flex-col w-8/12  ">
-            <div className="flex flex-col justify-around h-full">
-              <div className="mt-6 w-10/12 text-[1.5rem]">
+            <div className="flex flex-col justify-around ">
+              <div className="mt-6 w-10/12 text-[1.3rem]">
                 This is place where you can fund your ETH's to me.
                 <br />
                 Hoooraay!!!
@@ -61,14 +66,14 @@ const Main = ({ contract, wallet }) => {
 
               <div className="mt-4 w-full p-2 py-3 flex gap-4">
                 <input
-                  className="backdrop-blur-lg w-8/12 bg-opacity-10 bg-white outline-none p-6 text-3xl placeholder-slate-50/[0.5]"
+                  className="backdrop-blur-lg w-8/12 bg-opacity-10 bg-white outline-none p-4 text-2xl placeholder-slate-50/[0.5]"
                   type="text"
                   ref={value}
                   placeholder="add your eth amount"
                 />
                 <button
                   onClick={fund}
-                  className="p-3 w-4/12 bg-gradient-to-tr text-2xl from-green-400 to-blue-500 rounded-md"
+                  className="p-3 w-4/12 bg-gradient-to-tr text-1xl from-green-400 to-blue-500 rounded-md"
                 >
                   SEND
                 </button>
@@ -78,16 +83,38 @@ const Main = ({ contract, wallet }) => {
           <div className=" flex justify-center items-center ">
             <img
               src="eth.png"
-              style={{ width: 600 }}
+              style={{ width: 450 }}
               className="animate-pulse"
             />
           </div>
         </div>
-        <div className="flex flex-col text-[1rem] overflow-scroll  bg-slate-500 p-2 mt-3">
-          <span>to:{transactionReceipt["to"]}</span>
-          <span>from:{transactionReceipt["from"]}</span>
-          <span>contractAddress:{transactionReceipt["contractAddress"]}</span>
-          <span>blockHash:{transactionReceipt["blockHash"]}</span>
+        <div className="flex w-full  ">
+          {transactionReceipt && (
+            <div className="flex flex-col text-[1rem] w-full bg-slate-500  font-mono p-4 m-3">
+              <span>to:{transactionReceipt["to"]}</span>
+              <span>from:{transactionReceipt["from"]}</span>
+              <span>
+                contractAddress:{transactionReceipt["contractAddress"]}
+              </span>
+              <span>
+                transactionIndex:{transactionReceipt["transactionIndex"]}
+              </span>
+              <span>gasUsed:{transactionReceipt["gasUsed"].toString()}</span>
+              <span>blockHash:{transactionReceipt["blockHash"]}</span>
+              <span>
+                transaction Hash:{transactionReceipt["transactionHash"]}
+              </span>
+              <span>blockNumber:{transactionReceipt["blockNumber"]}</span>
+              <span>
+                cumulativeGasUsed:
+                {transactionReceipt["cumulativeGasUsed"].toString()}
+              </span>
+              <span>
+                effectiveGasPrice:
+                {transactionReceipt["effectiveGasPrice"].toString()}
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </div>
