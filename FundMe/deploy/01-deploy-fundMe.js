38de12fc5d2cfe997dcb2cd6c8b74da09dbbc850
Dexter <dexterdevmode@gmail.com>
2022-11-02 22:45:41 +0530

    Goerli Network added

--- FundMe/deploy/01-deploy-fundMe.js
+++ FundMe/deploy/01-deploy-fundMe.js
@@ -1,10 +1,20 @@
+const { networkConfig } = require("../helper.config");
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
 
   const { deployer } = await getNamedAccounts();
 
-  const mockV3Aggregator = await deployments.get("MockV3Aggregator");
-  const priceFeedAddress = mockV3Aggregator.address;
+  const chainId = network.config.chainId;
+
+  let priceFeedAddress;
+
+  if (developmentChains.includes(network.name)) {
+    const mockV3Aggregator = await deployments.get("MockV3Aggregator");
+    priceFeedAddress = mockV3Aggregator.address;
+  } else {
+    priceFeedAddress = networkConfig[chainId]["priceFeedAddress"];
+  }
 
   console.log("Deploying FundMe");
   const fundMe = await deploy("FundMe", {
--- FundMe/hardhat.config.js
+++ FundMe/hardhat.config.js
@@ -3,7 +3,21 @@ require("@nomiclabs/hardhat-ethers");
 require("hardhat-deploy");
 require("@nomiclabs/hardhat-waffle");
 require("solidity-coverage");
+require("dotenv").config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const EHTERSCAN_API_KEY = process.env.EHTERSCAN_API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      account: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
   solidity: {
     compilers: [{ version: "0.8.17" }, { version: "0.6.6" }],
   },
